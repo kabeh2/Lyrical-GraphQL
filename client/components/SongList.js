732e909f345129e684161c56a5ab822f8a75dbaa
kabeh2 <mattcnk@hotmail.com>
2020-09-04 13:05:05 -0400

    Added Query to see Data and Mutations to modify data

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(({ id, title }) => {
+      return (
+        <li key={id} className='collection-item'>
+          {title}
+          <i className='material-icons' onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
+        </li>
+      );
+    });
+  }
+
+  onSongDelete(id) {
+    this.props
+      .mutate({
+        variables: { id },
+      })
+      .then(() => this.props.data.refetch());
+  }
   render() {
     console.log(this.props);
+    if (this.props.data.loading) return <div>Loading...</div>;
 
-    return <div>SongList</div>;
+    return (
+      <div>
+        <ul className='collection'>{this.renderSongs()}</ul>
+        <Link to='/songs/new' className='btn-floating btn-large red right'>
+          <i className='material-icons'>add</i>
+        </Link>
+      </div>
+    );
   }
 }
 
-const query = gql`
-  {
-    songs {
-      title
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
     }
   }
 `;
 
-export default graphql(query)(SongList);
+export default graphql(mutation)(graphql(query)(SongList));
